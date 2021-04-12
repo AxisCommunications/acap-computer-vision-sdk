9b689510c268e1f6c900d3bd52e638aa4d9ae158
joha-axis <71760803+joha-axis@users.noreply.github.com>
2021-04-12 20:46:03 +0200

    add meta dir, enable non-buildkit (#2)
    * add support for non-buildkit builds
    
    * add top level meta dir
    
    * fix build script

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
+pushd sdk
+DOCKER_BUILDKIT=1 \
 docker build -f Dockerfile.armv7hf \
-    --progress=plain \
-    --build-arg http_proxy=http://wwwproxy.se.axis.com:3128 \
-    --build-arg https_proxy=http://wwwproxy.se.axis.com:3128 \
-    --build-arg UBUNTU_VERSION=20.04 \
     --build-arg NUMPY_BUILD_CORES=2 \
     --build-arg SCIPY_BUILD_CORES=2 \
     --build-arg OPENBLAS_BUILD_CORES=2 \
-    --target sdk \
-    -t acap-computer-vision-sdk:latest .
+    -t axisecp/acap-computer-vision-sdk:latest-armv7hf-ubuntu20.04 .
+popd
