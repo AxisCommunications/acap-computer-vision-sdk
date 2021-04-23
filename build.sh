5dca6e7f878a746ad1d281e5a4d554c92283acad
Johan Ahlkvist <joha@axis.com>
2021-04-23 15:09:43 +0200

    update default parallelization of scipy build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ pushd sdk
 DOCKER_BUILDKIT=1 \
 docker build -f Dockerfile.armv7hf \
     --build-arg NUMPY_BUILD_CORES=2 \
-    --build-arg SCIPY_BUILD_CORES=2 \
+    --build-arg SCIPY_BUILD_CORES=4 \
     --build-arg OPENBLAS_BUILD_CORES=2 \
     -t axisecp/acap-computer-vision-sdk:latest-armv7hf-ubuntu20.04 .
 popd
